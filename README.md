e6a49a976f25e296544005186ae3aa1b7e32d081
Yuchuan Huang <itisyuchuan@gmail.com>
2025-03-04 22:56:25 -0600

    update broadcast download

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
 # iharp-era5-data-download
-
-
-cd /export/scratch/huan1531/iharp-era5_data_download/
-
-source venv/bin/activate
-
-tmux new -d -s 2015 "python download.py -t download_2015.toml"
-tmux new -d -s 2016 "python download.py -t download_2016.toml"
-tmux new -d -s 2017 "python download.py -t download_2017.toml"
-tmux new -d -s 2018 "python download.py -t download_2018.toml"
-tmux new -d -s 2019 "python download.py -t download_2019.toml"
-tmux new -d -s 2020 "python download.py -t download_2020.toml"
-tmux new -d -s 2021 "python download.py -t download_2021.toml"
-tmux new -d -s 2022 "python download.py -t download_2022.toml"
-tmux new -d -s 2023 "python download.py -t download_2023.toml"
-tmux new -d -s 2024 "python download.py -t download_2024.toml"
--- broadcast.sh
+++ broadcast.sh
@@ -4,14 +4,20 @@
 for ((i=201; i<=210; i++)); do
     hostname="cs-spatial-${i}.cs.umn.edu"
     echo "Working on $hostname"
+    year=$((2015-201+$i))
     ssh huan1531@"$hostname" << EOF
-    cd /export/scratch
-    rm -rf huan1531
-    mkdir huan1531
-    cd huan1531
-    git clone https://github.com/iharp3/iharp-era5-data-download.git
-    cd iharp-era5-data-download
-    bash init_venv.sh
+        cd /export/scratch
+        rm -rf huan1531
+        mkdir huan1531
+        cd huan1531
+        git clone https://github.com/iharp3/iharp-era5-data-download.git
+        cd iharp-era5-data-download
+        bash init_venv.sh
+        source venv/bin/activate
+        echo "Downloading data for $year"
+        echo $year
+        # tmux kill-server
+        tmux new-session -d -s ${year} "python download.py -t download_${year}.toml" || echo "Failed to create tmux session for year ${year}"
 EOF
     echo "Done"
 done
